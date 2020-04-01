928fa8e9de753d6f1bf14b8bdc8b9c602c831131
insanityatitsfinest <3108543@gmail.com>
2020-03-31 20:13:38 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,475 @@
+﻿using System;
+
+namespace lover
+{
+    class Program
+    {
+      
+        
+
+       
+            static void Main(string[] args)  
+            {
+
+       
+           
+            int A = 0;
+                int B = 0;
+                int C = 0;
+                int D = 0;
+                int E = 0;
+                string q = "";
+            string a = "";
+                //string q1 = "";
+                string q2 = "";
+                string q3 = "";
+                string q4 = "";
+                string q5 = "";
+           
+                
+                Console.WriteLine("Love Languges Test");
+                Console.WriteLine("");
+            Console.WriteLine("Turn on CAPSLOCK");
+            Console.WriteLine("Question 1");
+            Console.WriteLine("My wife’s love notes make me feel good.(A)");
+                Console.WriteLine("I love my wife’s hugs.(E)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+                { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+
+            Console.WriteLine("Question 2");
+            Console.WriteLine("I like to be alone with my wife.(B)");
+                Console.WriteLine("I feel loved when my wife helps me do my work.(D)");
+                q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 3");
+            Console.WriteLine("Receiving special gifts from my wife make me happy..(C)");
+            Console.WriteLine("I enjoy long trips with my wife.(B)");
+            q = Console.ReadLine();
+                if (q == "A")
+                { A++; }
+                if (q == "B")
+                { B++; }
+                if (q == "C")
+                { C++; }
+                if (q == "D")
+                { D++; }
+                if (q == "E")
+                { E++; }
+
+
+            Console.WriteLine("Question 4");
+            Console.WriteLine("I feel loved when my wife does my laundry.(D)");
+            Console.WriteLine("I like it when my wife touches me.(E)");
+            q4 = Console.ReadLine();
+                if (q4 == "A")
+                { A++; }
+                if (q4 == "B")
+                { B++; }
+                if (q4 == "C")
+                { C++; }
+                if (q4 == "D")
+                { D++; }
+                if (q4 == "E")
+                { E++; }
+
+            Console.WriteLine("Question 5");
+            Console.WriteLine("I feel loved when my wife puts her arm around me.(E)");
+                Console.WriteLine("I know my wife loves me because she surprises me with gifts.(C)");
+            q5 = Console.ReadLine();
+                if (q5 == "A")
+                { A++; }
+                if (q5 == "B")
+                { B++; }
+                if (q5 == "C")
+                { C++; }
+                if (q5 == "D")
+                { D++; }
+                if (q5 == "E")
+                { E++; }
+            Console.WriteLine("Question 6");
+            Console.WriteLine("I Like going most anywhere with my wife.(B)");
+            Console.WriteLine("I like to hold my wife’s hand.(E)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 7");
+            Console.WriteLine("I value the gifts my wife gives to me.(C)");
+            Console.WriteLine("I love to hear my wife say she loves me.(A)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 8");
+            Console.WriteLine("I like for my wife to sit close to me.(E)");
+            Console.WriteLine("My wife tells me I look good, and I like that.(A)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 9");
+            Console.WriteLine("Spending time with my wife makes me happy.(B)");
+            Console.WriteLine("Even the smallest gift from my wife is important to me.(C)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 10");
+            Console.WriteLine("I feel loved when my wife tells me she is proud of me.(A)");
+            Console.WriteLine("When my wife cooks a meal for me, I know that she loves me.(D)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 11");
+
+            Console.WriteLine("Little things my wife does for me mean more to me than things she says.(D)");
+            Console.WriteLine("I love to hug my wife.(E)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 12");
+            Console.WriteLine("No matter what we do, I love doing things with my wife.(B)");
+            Console.WriteLine("Supportive comments from my wife make me feel good.(A)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+
+            Console.WriteLine("Question 13");
+            Console.WriteLine("My wife’s praise means a lot to me.(A)");
+            Console.WriteLine("It means a lot to me that my wife gives me gifts I really like.(C)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 14");
+            Console.WriteLine("Just being around my wife makes me feel good.(B)");
+            Console.WriteLine("I love it when my wife rubs my back.(E)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+
+            Console.WriteLine("Question 15");
+            Console.WriteLine("My wife’s reactions to my accomplishments are so encouraging.(A)");
+            Console.WriteLine("It means a lot to me when my wife helps with something I know she hates.(D)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 16");
+            Console.WriteLine("I never get tired of my wife’s kisses.(E)");
+            Console.WriteLine("I love that my wife shows real interest in things I like to do(B)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 17");
+            Console.WriteLine("I can count on my wife to help me with projects.(D)");            Console.WriteLine("I still get excited when opening a gift from my wife.(C)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 18");
+            Console.WriteLine("I love for my wife to compliment my appearance.(A)");            Console.WriteLine("I love that my wife listens to my ideas and doesn’t rush to judge or criticize.(B)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 19");
+            Console.WriteLine("I cant help but touch my wife when she's close by.(E)");            Console.WriteLine("My wife sometimes runs errands for me and i appreciate that.(D)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 20");    
+            Console.WriteLine("My wife deserves an award for all the things she does to help me.(D)");            Console.WriteLine("I’m sometimes amazed at how thoughtful my wife’s gifts to me are.(C)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 21");
+            Console.WriteLine("I love having my wife’s undivided attention.(B)");            Console.WriteLine("Keeping the house clean is an important act of service.(D)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 22");
+            Console.WriteLine("I look forward to seeing what my wife gives me for my birthday.(C)");            Console.WriteLine("I never get tired of hearing my wife tell me that I am important to her.(A)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 23");
+
+            Console.WriteLine("My wife lets me know she loves me by giving me gifts.(C)");            Console.WriteLine("My wife shows her love by helping me catch up on projects around the house.(D)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 24");
+
+            Console.WriteLine("My wife doesn’t interrupt me when I am talking, and I like that.(B)");            Console.WriteLine("I never get tired of receiving gifts from my wife.(C)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 25");
+
+            Console.WriteLine("My wife can tell when I’m tired, and she’s good about asking how she can help.(D)");            Console.WriteLine("It doesn’t matter where we go, I just like going places with my wife.(B)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 26");
+
+
+            Console.WriteLine("I love having sex with my wife.(E)");            Console.WriteLine("I love surprise gifts from my wife.(C)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 27");
+
+
+            Console.WriteLine("My wife’s encouraging words give me confidence.(A)");            Console.WriteLine("I love to watch movies with my wife.(B)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 28");
+
+            Console.WriteLine("I couldn’t ask for any better gifts than the ones my wife gives me.(C)");            Console.WriteLine("I just can’t keep my hands off my wife.(E)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+
+            Console.WriteLine("Question 29");
+
+            Console.WriteLine("It means a lot to me when my wife helps me despite having other things to do.(D)");            Console.WriteLine("It makes me feel really good when my wife tells me she appreciates me.(A)");
+            q = Console.ReadLine();
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+            Console.WriteLine("Question 30");
+
+            Console.WriteLine("I love hugging and kissing my wife after we’ve been apart 30 for a while.(E)");            Console.WriteLine("I love hearing my wife tell me that she believes in me.(A)");
+            if (q == "A")
+            { A++; }
+            if (q == "B")
+            { B++; }
+            if (q == "C")
+            { C++; }
+            if (q == "D")
+            { D++; }
+            if (q == "E")
+            { E++; }
+
+            Console.WriteLine("Words of Affirmation");
+            Console.WriteLine(A);
+            Console.WriteLine("Quality Time");
+                Console.WriteLine(B);
+            Console.WriteLine("Reciving Gifts");
+                Console.WriteLine(C);
+            Console.WriteLine("Acts of Service");
+                Console.WriteLine(D);
+            Console.WriteLine("Physical Touch");
+                Console.WriteLine(E);
+            }
+        }
+    }
+
+
+
+
